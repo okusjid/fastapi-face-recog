11acb8a818b5235cf238516e3af44c2cb687a855
okusjid <l200927@lhr.nu.edu.pk>
2024-09-08 13:41:52 +0500

    Refactor structure

--- src/celery_worker.py
+++ src/celery_worker.py
@@ -1,11 +1,11 @@
 from app.config.celery_config import celery_app
 import time
 
-# Face recognition task
-@celery_app.task
-def process_face_recognition(data):
-    time.sleep(5)  # Simulate a long-running task
-    return {"status": "Processed", "data": data}
+# # Face recognition task
+# @celery_app.task
+# def process_face_recognition(data):
+#     time.sleep(5)  # Simulate a long-running task
+#     return {"status": "Processed", "data": data}
 
 # Web crawler task
 @celery_app.task
